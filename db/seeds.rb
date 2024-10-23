fb1f4b77066a6e6e2a0ad8a4527eeae05f2f4a43
migz <miguel.mombay@deya.app>
2024-10-23 10:40:54 +0800

    update db config

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+# CONSTS
+EMPLOYEE_COUNT = 5
+
+
+company = Company.create!(name: Faker::Company.name)
+puts "created company with name: #{company.name}"
+
+
+project = ProjectItem.create!(name: Faker::App.name, company_id: company.id)
+puts "created project with name: #{project.name}"
+
+company.project_items << project
+
+# create users
+EMPLOYEE_COUNT.times do |i|
+    employee = Employee.create!({
+        first_name: Faker::Name.first_name,
+        middle_name: Faker::Name.middle_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        job_title: Faker::Job.title,
+        company_id: company.id
+    })
+
+    puts "created company with name: #{employee.first_name}"
+    
+    # place employees into created project
+
+    project.employees << employee
+
+    puts "added employee: #{employee.first_name} to project: #{project.name}"
+end
+
