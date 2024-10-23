51ea7761810c7579ab7f91478f5a717574c9e917
migz <miguel.mombay@deya.app>
2024-10-23 10:43:27 +0800

    add home button

---
+++ frontend/src/components/HomeButton.tsx
@@ -0,0 +1,9 @@
+import { NavLink } from "react-router-dom"
+
+export default function HomeButton () {
+    return(
+        <NavLink to="/" className="block w-1/3 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            Home
+        </NavLink>
+    )
+}
